ec50a1e81dfe8513b13aa5495c6608d221790ec6
atxrenegade <HarleighAbel@gmail.com>
2017-02-12 22:51:33 -0700

    first commit

---
+++ chap7.1_review.h
@@ -0,0 +1,14 @@
+// Harleigh Abel
+// Feb. 12, 2017
+
+// C Programming Absolute Beginner's Guide pg 62
+// chap7_ex1.h header file (my version)
+
+// If you have certain values that will not change (or only change
+// rarely)
+// you can set them with #DEFINE statements (so you can change them 
+// as needed)
+
+#define KIDS 3 
+#define FAMILY "The Mitchells"
+#define RENTAL_INCOME 2450
