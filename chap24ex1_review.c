9c6a3c47d394f8e829aca27587733b34460333c7
Harleigh Abel <harleighabel@gmail.com>
2017-05-06 22:35:42 -0600

    Update for 2017
    Update year and movie titles

--- chap24ex1_review.c
+++ chap24ex1_review.c
@@ -2,57 +2,90 @@
 Harleigh Abel
 May 3, 2017
 Absolute Beginners Guide to C Programming 
-Chapter 24 ex1 page 226
-chap23ex1_review.c
-This program demonstrates pointers by declaring and 
-initializing both regular and pointer variables for 
-int, float, and char types and then displays the values 
-of each. */
+Chapter 25 ex1 page 237
+chap25ex1_review.c
+This program declares and initializes an array of character 
+pointers and then asks for ratings associated */
 
 #include <stdio.h> 
 
  
 int main (void)
 {
-	int kids;
-	int * pKids;
-	float price;
-	float * pPrice;
-	char code;
-	char * pCode;
+	int i;
+	int ctr = 0;
+	char ans;
 
-	price = 17.50;
-	pPrice = &price;
+	// Declaring our array of 9 characters and then 
+	// initializing them
+	char * movies[9] = {"Arrival", "Fences",
+                                 "Hacksaw Ridge",
+                                 "Hell or High Water",
+                                 "Hidden Figures", 
+                                 "La La Land",
+                                 "Lion",
+                                 "Manchester by the Sea" 
+	                    };
 
-	printf("How many kids are you taking to the water park? ");
-	scanf(" %d", &kids);
-	
-	pKids = &kids;
+	int movieratings[9]; // A corresponding array of 9 integers
+	// for movie ratings
 
-	printf("\nDo you have a discount ticket for the park?");
-	printf("\nEnter E for Employee Discount, S for Sav-Mor ");
-	printf("Discount, or N for for No Discount: ");
-	scanf(" %c", &code);
+	  char * tempmovie = "This will be used to sort rated movies";
+	  int outer, inner, didSwap, temprating; // for the sort loop
 
-	printf("\nFirst let's do it with the variables:\n");
-	printf("You've got %d kids...\n", kids);
-	switch(code)
+	  printf("\n\n*** Oscar Season 2017 is here! (again) ***\n\n");
+	  printf("Time to rate this year's best picture nominees:");
+
+	  for (i = 0; i < 9; i++)
+	  {
+		  printf("\nDid you see %s? (Y/N): ", movies[i]);
+		  scanf(" %c", &ans);
+
+		  if ((toupper(ans)) == 'Y')
+		  {
+		    printf("\nWhat was your rating on a scale ");
+			  printf("of 1-10: ");
+			  scanf("%d ", &movieratings[i]);
+			  ctr++; // This will be used to print only movies
+				   // you've seen
+			  continue;
+		  }
+ 		  else
+ 		  {
+ 			  movieratings[i] = -1;
+ 		  }
+	  }	
+
+	// Now sort the movies by rating (the unseen will go
+	// to the bottom)
+
+	for (outer = 0; outer < 8; outer ++)
 	{
-		case ('E'):
-			printf("The Employee Discount saves you 25%% on the ");
-			printf("$%.2f price", price);
-			printf("\nTotal ticket cost: $%.2f", (*pPrice * .75 * *pKids));
-		break;
-
-		case('S'):
-			printf("The Sav-more Discount saves you 15%% on the ");
-			printf("$%.2f price", *pPrice);
-			printf("\nTotal ticket cost: $%.2f", (*pPrice * .85 * *pKids));
-		break;
-			default : // Either entered N for No Discount or
-			// an invalid letter
-			printf("You will be paying the full price of ");
-			printf("$%.2f for your tickets", *pPrice);
-	}		
-return 0;
-}
+		didSwap = 0;
+		for (inner = outer; inner < 9; inner++)
+		{
+			if (movieratings[inner] > movieratings[outer])
+			{
+				tempmovie = movies[inner];
+				temprating = movieratings[inner];
+				movies[inner] = movies[outer];
+				movieratings[inner] = movieratings[outer];
+				movies[outer] = tempmovie;
+				movieratings[outer] = temprating;
+				didSwap = 1;
+			}
+		}
+		if (didSwap == 0)
+		{
+			break;
+		}
+	}
+	// Now print the movies you saw in order
+	printf("\n\n** Your Movie Ratings for the 2012 Oscar ");
+	printf("Contenders **\n");
+	for (i = 0; i < ctr ; i++)
+	{
+		printf("%s rated a %d:\n", movies[i], movieratings[i]);
+	}
+	return 0;
+}	
