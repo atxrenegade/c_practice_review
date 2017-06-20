358fb4ceaf134fac3ecab84e840b7317cbc93f0d
Harleigh Abel <harleighabel@gmail.com>
2017-06-20 00:10:46 -0500

    corrected bugs and typos
     Removed duplicate comment marking
     typo "return", typo "functions", typo "the"
    added missing comment mark

--- chap32ex2_review.c
+++ chap32ex2_review.c
@@ -2,10 +2,9 @@
 Harleigh Abel
 April 27, 2017
 Absolute Beginners Guide to C Programming 
-Chapter 32 ex1 page 297
+Chapter 32 ex2 page 297
 chap32ex2_review.c
-
-/* The program demonstrates passing an array to a function. */
+The program demonstrates passing an array to a function. */
 
 #include <stdio.h>
 #include <string.h>
@@ -15,21 +14,21 @@ int main (void)
     char name[15] = "Michael Hatton";
     change(name);
     
-    printf("Back in main(), tha name is now %s. \n", name);
+    printf("Back in main(), the name is now %s.\n", name);
     
-    retrun(0);  // Ends the program
+    return(0);  // Ends the program
     
 }
 
 /****************************************/
 
 /* Sometimes putting dividers like the one above
-is a nice break between your different fucntions. */
+is a nice break between your different functions. */
 
 change(char name[15])   // Recieves the value of i
 {
     // Change the string stored at the address 
-    pointed to by name
+    // pointed to by name
     
     strcpy(name, "XXXXXXXXXXXXXXXXXXXXXXXX");
     return; // Returns to main
