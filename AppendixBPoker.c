617ad0923854361a6162571a71aa7ec9e45ef273
Harleigh Abel <harleighabel@gmail.com>
2017-06-26 18:30:39 -0500

    add program variables and execution
    add program variables and execution of poker game

--- AppendixBPoker.c
+++ AppendixBPoker.c
@@ -37,7 +37,77 @@ void getFinalHand(int cardRank[], int cardSuit[], int finalRank[], int finalSuit
 
 int main(void)
 {
+	int bet;
+	int bank = 100;
+	int i;
+	int cardRank[5]; // Will be one of 13 values ACE-KING
+	int cardSuit[5]; // Will be one of 4 values (for Clubs, Diamonds,
+					// Hearts, Spades)
+	int ranksinHand[13]; // Used for evaulating the final hand
+	int suitsinHand[4]; // Used for evaulating the final hand
+	int winnings;
+	time_t t;
+	char suit, rank, stillPlay;
+
+	// This function is called outside the do...while	
+	// while loop because the greeting only needs to
+	// be displayed once, while everything else in main
+	// will run multiple times, depending on how many
+	// times the user wants to play.
+
+	printGreeting();
+
+	// Loop runs each time the user plays a hand of draw poker
+
+	do {
+		bet = getBet();
+		srand(time(&t));
+		getFirstHand(cardRank, cardSuit);
+		printf("Your five cards: \n");
+		for(i = 0; i < 5; i++)
+		{
+			suit = getSuit(cardSuit[i]);
+			rank = getRank(cardRank[i]);
+			printf("Card #%d: %c%c\n", i + 1, rank, suit);
+		}
+
+		// These two arrays are used to figure out the value
+		// of the player's hand. However, they must be zeroed out
+		// in case the user plays multiple hands.
+
+		for (i = 0; i < 4; i++)
+		{
+			suitsinHand[i] = 0;
+		}
+
+		for (i = 0; i < 4; i++)
+		{
+			ranksinHand[i] = 0;
+		}	
+
+		getFinalHand(cardRank, cardSuit, finalRank, finalSuit, ranksinHand, suitsinHand);
+
+		printf("Your five final cards: \n");
+		for(i = 0; i < 5; i++)
+		{
+			suit = getSuit(finalSuit[i]);
+			rank = getRank(finalRank[i]);
+			printf("Card #%d: %c%c\n", i + 1; rank, suit);
+		}
+		 	// possible missing curly brace
+			winnings = analyzeHand(ranksinHand, suitsinHand);
+			printf("You won %d!\n", bet * winnings);
+			bank = bank - bet + (bet * winnings);
+			printf("\nYour bank is now %d.\n", bank);
+			printf("\nDo you want to play again? ");
+
+			scanf(" %c", &stillPlay);
+		} while (toupper(stillPlay) == 'Y');
+		
+			return;
+	}
 
 	
 }
 
+ 
