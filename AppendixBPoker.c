cecbf82c52284104af84edb7efdbeaedba48b3a1
Harleigh Abel <harleighabel@gmail.com>
2017-06-29 23:32:26 -0500

    add functions getSuit() and getRank()
    Add function that changes the suit integer value to a character
    Add function that changes rank integer value to c character

--- AppendixBPoker.c
+++ AppendixBPoker.c
@@ -167,5 +167,60 @@ int main(void)
 		}	
 	}
 
+	// Function that changes the suit integer value to a character
+	// representing the suit
+
+	char getSuit(int suit)
+	{
+		switch (suit)
+		{
+			case 0:
+				return('c');
+			case 1:
+				return('d');
+			case 2:	
+				return('h');
+			case 3:
+				return('s');			
+		}
+	}
+
+// Function that changes the rank integer value to a character
+// representing the rank
+
+char getRank(int rank)
+{
+	switch (rank)
+	{
+		case 0:
+			return('A');
+		case 1:
+			return('2');
+		case 2:
+			return('3');
+		case 3:
+			return('4');
+		case 4:
+			return('5');
+		case 5:
+			return('6');
+		case 6:
+			return('7');
+		case 7:
+			return('8');
+		case 8:
+			return('9');
+		case 9:
+			return('T');
+		case 10:
+			return('J');
+		case 11:
+			return('Q');
+		case 12:
+			return('K');		
+
+	}
+}	
 	
+
 }
